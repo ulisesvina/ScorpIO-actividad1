7d4042727750314327a18f6c35671afd61be31be
BelenDiaz-web <belen_diaz777@ciencias.unam.mx>
2026-09-23 19:44:49 -0600

    Agrego pagina de presentacion de Belen

--- presentacion/views.py
+++ presentacion/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def pagina_belen(request):
+    return HttpResponse(
+        "<h1>Belén</h1>"
+        "<p>Rol en ScorpIO: Responsable y Colaboración</p>"
+    )
