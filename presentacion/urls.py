18412d4c6c0de0b6595c6a8221afecc5276b83ed
lunaecl <lunaemiliano780@gmail.com>
2026-09-23 22:19:16 -0600

    feat: agrega vista de presentación de Tu Nombre

--- presentacion/urls.py
+++ presentacion/urls.py
@@ -5,3 +5,7 @@ from . import views
 urlpatterns = [
     path("belen/", views.pagina_belen, name="pagina_belen"),
 ]
+urlpatterns = [
+    path('belen/', views.presentacion_belen, name='presentacion_belen'),   # ya existente, no borrar
+    path('tunombre/', views.presentacion_tunombre, name='presentacion_tunombre'),
+]
--- presentacion/views.py
+++ presentacion/views.py
@@ -6,3 +6,14 @@ def pagina_belen(request):
         "<h1>Belén</h1>"
         "<p>Rol en ScorpIO: Responsable y Colaboración</p>"
     )
+def presentacion_tunombre(request):
+    html = """
+    <html>
+        <head><title>Página de presentación</title></head>
+        <body>
+            <h1>Emiliano</h1>
+            <p>Rol en ScorpIO: Responsablel</p>
+        </body>
+    </html>
+    """
+    return HttpResponse(html)
