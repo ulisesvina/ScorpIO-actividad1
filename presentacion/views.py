a84961cd95c080b0edbaed345c298e7aa10fbc56
Eduardo Vargas <eduardovargas21@ciencias.unam.mx>
2026-09-23 22:50:51 -0600

    se agrego presentación de Eduardo

--- presentacion/views.py
+++ presentacion/views.py
@@ -6,3 +6,9 @@ def pagina_belen(request):
         "<h1>Belén</h1>"
         "<p>Rol en ScorpIO: Responsable y Colaboración</p>"
     )
+
+def pagina_eduardo(request):
+    return HttpResponse(
+        "<h1>Eduardo</h1>"
+        "<p>Rol en ScorpIO: Responsable de calidad </p>"
+    )
